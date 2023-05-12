0c694b9ffa3a8e0eb7f7cd23e29344b95678f666
NonSwag <mrminecraft00@gmail.com>
2023-05-13 00:38:49 +0200

    implemented api changes

--- paper-plugin/src/main/java/net/thenextlvl/commander/implementation/CraftCommandManager.java
+++ paper-plugin/src/main/java/net/thenextlvl/commander/implementation/CraftCommandManager.java
@@ -2,6 +2,7 @@ package net.thenextlvl.commander.implementation;
 
 import net.thenextlvl.commander.api.CommandManager;
 import org.bukkit.Bukkit;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 
@@ -11,11 +12,24 @@ public class CraftCommandManager extends CommandManager {
     }
 
     @Override
-    public void unregisterCommand(String label, boolean alias) {
+    public void unregisterCommand(String label) {
+        var name = resolveCommandName(label);
+        if (name != null) Bukkit.getCommandMap().getKnownCommands().entrySet().stream()
+                .filter(entry -> entry.getValue().getName().equals(name))
+                .toList()
+                .forEach(entry -> {
+                    Bukkit.getCommandMap().getKnownCommands().remove(entry.getKey());
+                    entry.getValue().unregister(Bukkit.getCommandMap());
+                });
+    }
+
+    @Override
+    public boolean isCommandRegistered(String command) {
+        return Bukkit.getCommandMap().getKnownCommands().containsKey(command);
+    }
+
+    public @Nullable String resolveCommandName(String label) {
         var command = Bukkit.getCommandMap().getCommand(label);
-        if (command == null) return;
-        if (!alias) command.getAliases().forEach(s -> unregisterCommand(s, true));
-        Bukkit.getCommandMap().getKnownCommands().remove(label);
-        command.unregister(Bukkit.getCommandMap());
+        return command != null ? command.getName() : null;
     }
 }
--- paper-plugin/src/main/java/net/thenextlvl/commander/implementation/CraftPermissionManager.java
+++ paper-plugin/src/main/java/net/thenextlvl/commander/implementation/CraftPermissionManager.java
@@ -5,17 +5,38 @@ import org.bukkit.Bukkit;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class CraftPermissionManager extends PermissionManager {
+    private final Map<String, String> originalPermissions = new HashMap<>();
+
     public CraftPermissionManager(File dataFolder) {
         super(dataFolder);
     }
 
     @Override
-    public void overridePermission(String command, @Nullable String permission, boolean alias) {
+    public boolean overridePermission(String command, @Nullable String permission, boolean alias) {
         var target = Bukkit.getCommandMap().getCommand(command);
-        if (target == null) return;
+        if (target == null) return false;
         if (!alias) target.getAliases().forEach(s -> overridePermission(s, permission, true));
+        originalPermissions.putIfAbsent(target.getName(), target.getPermission());
+        if (Objects.equals(target.getPermission(), permission)) return false;
         target.setPermission(permission);
+        return true;
+    }
+
+    @Override
+    public @Nullable String getOriginalPermission(String label) {
+        var command = Bukkit.getCommandMap().getCommand(label);
+        return command != null ? originalPermissions.get(command.getName()) : null;
+    }
+
+    @Override
+    public boolean hasOriginalPermission(String label) {
+        if (originalPermissions.containsKey(label)) return true;
+        var command = Bukkit.getCommandMap().getCommand(label);
+        return command != null && originalPermissions.containsKey(command.getName());
     }
 }
