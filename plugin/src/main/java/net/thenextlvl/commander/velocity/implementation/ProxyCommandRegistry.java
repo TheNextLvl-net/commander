a33930dee5d78dc54b5fbcb5fa4f31269561bca8
david <mrminecraft00@gmail.com>
2024-07-24 23:03:49 +0200

    update command registry

---
+++ plugin/src/main/java/net/thenextlvl/commander/velocity/implementation/ProxyCommandRegistry.java
@@ -0,0 +1,59 @@
+package net.thenextlvl.commander.velocity.implementation;
+
+import core.annotation.MethodsReturnNotNullByDefault;
+import core.annotation.ParametersAreNotNullByDefault;
+import core.annotation.TypesAreNotNullByDefault;
+import net.thenextlvl.commander.api.CommandRegistry;
+import net.thenextlvl.commander.velocity.CommanderPlugin;
+
+import java.util.Set;
+
+@TypesAreNotNullByDefault
+@MethodsReturnNotNullByDefault
+@ParametersAreNotNullByDefault
+public record ProxyCommandRegistry(CommanderPlugin plugin) implements CommandRegistry {
+
+    private com.velocitypowered.api.command.CommandManager getCommandManager() {
+        return plugin.server().getCommandManager();
+    }
+
+    @Override
+    public Set<String> hiddenCommands() {
+        return Set.of();
+    }
+
+    @Override
+    public Set<String> unregisteredCommands() {
+        return Set.of();
+    }
+
+    @Override
+    public boolean hide(String command) {
+        return false;
+    }
+
+    @Override
+    public boolean isHidden(String command) {
+        return false;
+    }
+
+    @Override
+    public boolean isUnregistered(String command) {
+        return false;
+    }
+
+    @Override
+    public boolean register(String command) {
+        return false;
+    }
+
+    @Override
+    public boolean reveal(String command) {
+        return false;
+    }
+
+    @Override
+    public boolean unregister(String command) {
+        return false;
+    }
+}
