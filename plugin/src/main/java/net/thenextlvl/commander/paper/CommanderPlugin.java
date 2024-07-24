c2b7c469621d1478ce1e2e117f2c3ec1740ecb58
david <mrminecraft00@gmail.com>
2024-07-24 22:56:47 +0200

    move commander api implementation inside commander plugin

--- plugin/src/main/java/net/thenextlvl/commander/paper/CommanderPlugin.java
+++ plugin/src/main/java/net/thenextlvl/commander/paper/CommanderPlugin.java
@@ -1,28 +1,50 @@
 package net.thenextlvl.commander.paper;
 
+import core.i18n.file.ComponentBundle;
 import lombok.Getter;
 import lombok.experimental.Accessors;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.thenextlvl.commander.api.CommandRegistry;
 import net.thenextlvl.commander.api.Commander;
 import net.thenextlvl.commander.paper.command.CommanderCommand;
-import net.thenextlvl.commander.paper.implementation.CraftCommander;
+import net.thenextlvl.commander.paper.implementation.PaperCommandRegistry;
+import net.thenextlvl.commander.paper.implementation.PaperPermissionOverride;
 import net.thenextlvl.commander.paper.listener.CommandListener;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.util.Locale;
+
 @Getter
 @Accessors(fluent = true)
-public class CommanderPlugin extends JavaPlugin {
-    private final CraftCommander commander = new CraftCommander(getDataFolder());
+public class CommanderPlugin extends JavaPlugin implements Commander {
+    private final File translations = new File(getDataFolder(), "translations");
+    private final ComponentBundle bundle = new ComponentBundle(translations, audience ->
+            audience instanceof Player player ? player.locale() : Locale.US)
+            .register("commander", Locale.US)
+            .register("commander_german", Locale.GERMANY)
+            .miniMessage(bundle -> MiniMessage.builder().tags(TagResolver.resolver(
+                    TagResolver.standard(),
+                    Placeholder.component("prefix", bundle.component(Locale.US, "prefix"))
+            )).build());
+
+    private final CommandRegistry commandRegistry = new PaperCommandRegistry();
+    private final PaperCommandRegistry commandManager = new PaperCommandRegistry();
+    private final PaperPermissionOverride permissionOverride = new PaperPermissionOverride();
 
     @Override
     public void onLoad() {
-        Bukkit.getServicesManager().register(Commander.class, commander, this, ServicePriority.Normal);
+        Bukkit.getServicesManager().register(Commander.class, this, this, ServicePriority.Highest);
     }
 
     @Override
     public void onEnable() {
-        Bukkit.getGlobalRegionScheduler().execute(this, () -> commander.permissionManager().overridePermissions());
+        Bukkit.getGlobalRegionScheduler().execute(this, () -> permissionOverride().overridePermissions());
         registerListeners();
         registerCommands();
     }
--- plugin/src/main/java/net/thenextlvl/commander/velocity/CommanderPlugin.java
+++ plugin/src/main/java/net/thenextlvl/commander/velocity/CommanderPlugin.java
@@ -5,15 +5,25 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.plugin.annotation.DataDirectory;
+import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
+import core.i18n.file.ComponentBundle;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
+import net.thenextlvl.commander.api.CommandRegistry;
+import net.thenextlvl.commander.api.Commander;
+import net.thenextlvl.commander.api.PermissionOverride;
+import net.thenextlvl.commander.velocity.implementation.ProxyCommandRegistry;
 import net.thenextlvl.commander.velocity.listener.CommandListener;
 import lombok.Getter;
 import lombok.experimental.Accessors;
 import net.thenextlvl.commander.velocity.command.CommanderCommand;
-import net.thenextlvl.commander.velocity.implementation.ProxyCommander;
 import org.slf4j.Logger;
 
+import java.io.File;
 import java.nio.file.Path;
+import java.util.Locale;
 
 @Getter
 @Accessors(fluent = true)
@@ -22,7 +32,9 @@ import java.nio.file.Path;
         authors = "NonSwag",
         url = "https://thenextlvl.net",
         version = "3.0.4")
-public class CommanderPlugin {
+public class CommanderPlugin implements Commander {
+    private final ComponentBundle bundle;
+    private final CommandRegistry commandRegistry;
     private final ProxyServer server;
     private final Logger logger;
     private final Path dataFolder;
@@ -32,12 +44,25 @@ public class CommanderPlugin {
         this.server = server;
         this.logger = logger;
         this.dataFolder = dataFolder;
+        this.bundle = new ComponentBundle(new File(dataFolder.toFile(), "translations"), audience ->
+                audience instanceof Player player ? player.getPlayerSettings().getLocale() : Locale.US)
+                .register("commander", Locale.US)
+                .register("commander_german", Locale.GERMANY)
+                .miniMessage(bundle -> MiniMessage.builder().tags(TagResolver.resolver(
+                        TagResolver.standard(),
+                        Placeholder.component("prefix", bundle.component(Locale.US, "prefix"))
+                )).build());
+        this.commandRegistry = new ProxyCommandRegistry(this);
+    }
+
+    @Override
+    public PermissionOverride permissionOverride() throws UnsupportedOperationException {
+        throw new UnsupportedOperationException("Not supported on the proxy");
     }
 
     @Subscribe
     public void onProxyInitialize(ProxyInitializeEvent event) {
-        var commander = new ProxyCommander(this);
-        server().getEventManager().register(this, new CommandListener(commander));
-        server().getCommandManager().register("v-command", new CommanderCommand(commander));
+        server().getEventManager().register(this, new CommandListener(this));
+        server().getCommandManager().register("v-command", new CommanderCommand(this));
     }
 }
