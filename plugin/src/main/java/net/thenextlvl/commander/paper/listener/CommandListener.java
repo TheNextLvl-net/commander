b6056835ff998b01d10a662319f9e1d3f494531b
david <mrminecraft00@gmail.com>
2024-07-24 22:57:09 +0200

    removed redundant listeners

--- plugin/src/main/java/net/thenextlvl/commander/paper/listener/CommandListener.java
+++ plugin/src/main/java/net/thenextlvl/commander/paper/listener/CommandListener.java
@@ -1,76 +1,20 @@
 package net.thenextlvl.commander.paper.listener;
 
 import lombok.RequiredArgsConstructor;
-import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.util.TriState;
-import net.thenextlvl.commander.api.CommandInfo;
-import net.thenextlvl.commander.paper.CommanderPlugin;
-import org.bukkit.entity.Player;
+import net.thenextlvl.commander.api.Commander;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.command.UnknownCommandEvent;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerCommandSendEvent;
-import org.bukkit.event.server.ServerCommandEvent;
 
 @RequiredArgsConstructor
 public class CommandListener implements Listener {
-    private final CommanderPlugin plugin;
+    private final Commander commander;
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onCommandSend(PlayerCommandSendEvent event) {
-        event.getCommands().removeIf(literal -> plugin.commander().commandRegistry()
-                .containsCommandInfo(info -> isHidden(event.getPlayer(), literal, info)));
-    }
-
-    private boolean isHidden(Player player, String literal, CommandInfo info) {
-        return info.status() != null && info.nameMatches(literal) &&
-               (!info.isHidden() || !player.permissionValue("commander.bypass").equals(TriState.TRUE));
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onCommand(ServerCommandEvent event) {
-        var literal = event.getCommand().split(" ")[0];
-        if (plugin.commander().commandManager().isCommandRegistered(literal)
-            && !plugin.commander().commandRegistry().isRemoved(literal)) return;
-        plugin.commander().bundle().sendMessage(event.getSender(), "command.unknown",
-                Placeholder.parsed("command", literal));
-        event.setCancelled(true);
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST)
-    public void onUnknownCommand(UnknownCommandEvent event) {
-        var literal = event.getCommandLine().split(" ")[0];
-        if (plugin.commander().commandManager().isCommandRegistered(literal)) return;
-        event.message(plugin.commander().bundle().component(event.getSender(), "command.unknown",
-                Placeholder.parsed("command", literal)));
-    }
-
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onCommand(PlayerCommandPreprocessEvent event) {
-        var player = event.getPlayer();
-        var literal = event.getMessage().substring(1).split(" ")[0];
-        var command = plugin.commander().commandManager().getCommand(literal).orElse(null);
-        if (command == null || plugin.commander().commandRegistry().isRemoved(literal)) {
-            event.setCancelled(true);
-            if (literal.isBlank()) return;
-            plugin.commander().bundle().sendMessage(player, "command.unknown",
-                    Placeholder.parsed("command", literal));
-        } else if (!command.testPermissionSilent(player)) {
-            var permission = command.getPermission() != null ? command.getPermission() : getPermission(literal);
-            if (permission != null) plugin.commander().bundle().sendMessage(player, "command.permission",
-                    Placeholder.parsed("permission", permission),
-                    Placeholder.parsed("command", literal));
-            else plugin.commander().bundle().sendMessage(player, "command.permission.unknown",
-                    Placeholder.parsed("command", literal));
-            event.setCancelled(true);
-        }
-    }
-
-    private String getPermission(String literal) {
-        return plugin.commander().commandRegistry().getCommandInformation(literal)
-                .map(CommandInfo::permission)
-                .orElse(null);
+        if (event.getPlayer().permissionValue("commander.bypass").equals(TriState.TRUE)) return;
+        event.getCommands().removeAll(commander.commandRegistry().hiddenCommands());
     }
 }
--- plugin/src/main/java/net/thenextlvl/commander/velocity/listener/CommandListener.java
+++ plugin/src/main/java/net/thenextlvl/commander/velocity/listener/CommandListener.java
@@ -1,34 +1,20 @@
 package net.thenextlvl.commander.velocity.listener;
 
 import com.velocitypowered.api.event.Subscribe;
-import com.velocitypowered.api.event.command.CommandExecuteEvent;
 import com.velocitypowered.api.event.command.PlayerAvailableCommandsEvent;
 import com.velocitypowered.api.permission.Tristate;
-import com.velocitypowered.api.proxy.Player;
 import lombok.RequiredArgsConstructor;
-import net.thenextlvl.commander.api.CommandInfo;
-import net.thenextlvl.commander.velocity.implementation.ProxyCommander;
+import net.thenextlvl.commander.velocity.CommanderPlugin;
 
 @RequiredArgsConstructor
 public class CommandListener {
-    private final ProxyCommander commander;
+    private final CommanderPlugin commander;
 
     @Subscribe
     @SuppressWarnings("UnstableApiUsage")
     public void onCommandSend(PlayerAvailableCommandsEvent event) {
-        event.getRootNode().getChildren().removeIf(commandNode -> commander.commandRegistry()
-                .containsCommandInfo(info -> isHidden(event.getPlayer(), commandNode.getName(), info)));
-    }
-
-    private boolean isHidden(Player player, String literal, CommandInfo info) {
-        return info.status() != null && info.nameMatches(literal) &&
-                (!info.isHidden() || !player.getPermissionValue("commander.bypass").equals(Tristate.TRUE));
-    }
-
-    @Subscribe
-    public void onCommand(CommandExecuteEvent event) {
-        var literal = event.getCommand().split(" ")[0];
-        if (!commander.commandRegistry().isRemoved(literal)) return;
-        event.setResult(CommandExecuteEvent.CommandResult.forwardToServer());
+        if (event.getPlayer().getPermissionValue("commander.bypass").equals(Tristate.TRUE)) return;
+        event.getRootNode().getChildren().removeIf(commandNode ->
+                commander.commandRegistry().isHidden(commandNode.getName()));
     }
 }
