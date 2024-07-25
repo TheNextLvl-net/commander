713090c2d2f2914a72d7e0010f330ef3484ef0bd
david <mrminecraft00@gmail.com>
2024-07-25 18:43:18 +0200

    Rename and refactor Gradle build script, remove Velocity config

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -4,8 +4,8 @@ import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
 plugins {
     id("java")
     id("io.github.goooler.shadow") version "8.1.7"
-    id("net.minecrell.plugin-yml.paper") version "0.6.0"
     id("io.papermc.hangar-publish-plugin") version "0.1.2"
+    id("net.minecrell.plugin-yml.paper") version "0.6.0"
     id("com.modrinth.minotaur") version "2.+"
 }
 
@@ -27,7 +27,6 @@ dependencies {
     compileOnly("org.projectlombok:lombok:1.18.32")
     compileOnly("net.thenextlvl.core:annotations:2.0.1")
     compileOnly("io.papermc.paper:paper-api:1.21-R0.1-SNAPSHOT")
-    compileOnly("com.velocitypowered:velocity-api:3.3.0-SNAPSHOT")
 
     implementation(project(":api"))
     implementation("net.thenextlvl.core:files:1.0.5")
@@ -68,13 +67,6 @@ hangarPublish { // docs - https://docs.papermc.io/misc/hangar-publishing
         apiKey.set(System.getenv("HANGAR_API_TOKEN"))
         platforms.register(Platforms.PAPER) {
             jar.set(tasks.shadowJar.flatMap { it.archiveFile })
-            platformVersions.set(versions)
-        }
-        platforms.register(Platforms.VELOCITY) {
-            jar.set(tasks.shadowJar.flatMap { it.archiveFile })
-            val versions: List<String> = (property("velocityVersion") as String)
-                .split(",")
-                .map { it.trim() }
             platformVersions.set(versions)
         }
     }
