64d128bb39b49d5ea758d8694c47975f58a8377a
david <mrminecraft00@gmail.com>
2024-07-27 11:19:46 +0200

    Set PostOrder.LAST for PlayerAvailableCommandsEvent handling

--- velocity/src/main/java/net/thenextlvl/commander/velocity/listener/CommandListener.java
+++ velocity/src/main/java/net/thenextlvl/commander/velocity/listener/CommandListener.java
@@ -1,5 +1,6 @@
 package net.thenextlvl.commander.velocity.listener;
 
+import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.command.PlayerAvailableCommandsEvent;
 import com.velocitypowered.api.permission.Tristate;
@@ -10,7 +11,7 @@ import net.thenextlvl.commander.velocity.CommanderPlugin;
 public class CommandListener {
     private final CommanderPlugin commander;
 
-    @Subscribe
+    @Subscribe(order = PostOrder.LAST)
     @SuppressWarnings("UnstableApiUsage")
     public void onCommandSend(PlayerAvailableCommandsEvent event) {
         if (event.getPlayer().getPermissionValue("commander.bypass").equals(Tristate.TRUE)) return;
