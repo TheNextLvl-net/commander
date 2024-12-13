5475dcfca1ba35810cc81c2228d0a799c0e4e188
david <mrminecraft00@gmail.com>
2024-12-13 20:20:12 +0100

    [ci skip] Exclude Gson from version-checker dependency in Velocity.
    This change ensures that Gson is no longer included as a transitive dependency for version-checker. It helps avoid potential conflicts with other Gson versions in the project. The rest of the build dependencies remain unaffected.

--- velocity/build.gradle.kts
+++ velocity/build.gradle.kts
@@ -33,7 +33,9 @@ dependencies {
     implementation("org.bstats:bstats-velocity:3.1.0")
     implementation("net.thenextlvl.core:files:2.0.0")
     implementation("net.thenextlvl.core:i18n:1.0.20")
-    implementation("net.thenextlvl.core:version-checker:2.0.1")
+    implementation("net.thenextlvl.core:version-checker:2.0.1") {
+        exclude("com.google.code.gson")
+    }
 
     annotationProcessor("org.projectlombok:lombok:1.18.36")
     annotationProcessor("com.velocitypowered:velocity-api:3.4.0-SNAPSHOT")
