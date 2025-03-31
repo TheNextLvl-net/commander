f08b2675a5d84e61117c8826353505a47170f237
david <github@nonswag.dev>
2025-03-31 21:18:29 +0200

    [ci skip] Updated `plugin-yml.paper` to version 0.7.1
    Switched to `de.eldoria` namespace and upgraded the plugin version.

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("com.gradleup.shadow")
     id("com.modrinth.minotaur")
     id("io.papermc.hangar-publish-plugin")
-    id("net.minecrell.plugin-yml.paper") version "0.6.0"
+    id("de.eldoria.plugin-yml.paper") version "0.7.1"
 }
 
 group = rootProject.group
