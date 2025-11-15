e961375513caaec9436d3893627b10bb97c83b1f
david <mail@nonswag.dev>
2025-11-15 11:37:34 +0100

    Refined permission override and command listener

--- paper/src/main/java/net/thenextlvl/commander/paper/listener/CommandListener.java
+++ paper/src/main/java/net/thenextlvl/commander/paper/listener/CommandListener.java
@@ -1,10 +1,15 @@
 package net.thenextlvl.commander.paper.listener;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 import net.kyori.adventure.util.TriState;
 import net.thenextlvl.commander.paper.CommanderPlugin;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerCommandSendEvent;
 import org.jspecify.annotations.NullMarked;
 
@@ -24,5 +29,19 @@ public class CommandListener implements Listener {
             var permission = plugin.commons.permissionOverride().permission(command);
             return permission != null && !event.getPlayer().hasPermission(permission);
         });
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
+        var noSlash = event.getMessage().substring(1);
+        var command = noSlash.split(" ", 2)[0];
+        var permission = plugin.commons.permissionOverride().permission(command);
+        if (permission == null || event.getPlayer().hasPermission(permission)) return;
+        event.getPlayer().sendMessage(Component.translatable("command.unknown.command").appendNewline()
+                .append(Component.text(noSlash).decorate(TextDecoration.UNDERLINED)
+                        .append(Component.translatable("command.context.here").decorate(TextDecoration.ITALIC))
+                        .clickEvent(ClickEvent.suggestCommand(event.getMessage())))
+                .color(NamedTextColor.RED));
+        event.setCancelled(true);
     }
 }
